faebf9649e37fd669c9c8d0a5104c1f4fa6c3176
zengame6817-cell <zen.game.6817@gmail.com>
2026-08-23 13:03:05 +0900

    3.0.32

---
+++ js/config-v3.0.32.js
@@ -0,0 +1,8 @@
+"use strict";
+window.IRUNA_CONFIG = Object.freeze({
+  API_URL: "https://script.google.com/macros/s/AKfycbzZ3XbV1kS3zm8KYx4Ou3CeUzbVrVVUnixewJ2ZqU5zdyzxDIAev5rPTCKBoje2YkiL3g/exec",
+  STATIC_DB_URL: "./data/db.json",
+  APP_VERSION: "3.0.32",
+  API_TIMEOUT_MS: 20000,
+  STATIC_DB_TIMEOUT_MS: 30000
+});
